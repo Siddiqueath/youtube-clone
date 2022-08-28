c5017b9c47509c7466d393481b20a0e69a7e5024
siddiqueath <MY_NAME@example.com>
2022-08-28 12:23:35 +0530

    video's fetched

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
-let searchBar = document.querySelector('#search-bar'); /* element to trigger hide */
+/* for searchIcon in search bar */
+const searchBar = document.querySelector('#search-bar'); /* element to trigger hide */
 searchBar.addEventListener('focus', showSearchImg);
 searchBar.addEventListener('blur', showSearchImg);
 
-let searchImg = document.querySelector('#center > img'); /* element to hide */
+const searchImg = document.querySelector('#center > img'); /* element to hide */
 function showSearchImg() {
     searchImg.style.visibility == 'visible'? 
         searchImg.style.visibility='hidden': searchImg.style.visibility='visible';
 }
 
-// const videoContainer = document.querySelector('main');
-// let apiKey = 'AIzaSyCFYqgxOa6P18e-PBEy2jvQQDBnuKzxbsE';
-// let videoHttp = 'https://www.googleapis.com/youtube/v3/videos?';
-// let channelHttp = 'https://www.googleapis.com/youtube/v3/channels?';
+/* for nav bar */
+const burgerMenu = document.querySelector('#burger-menu');
+burgerMenu.addEventListener('click', toggleSideBar);
+const sideBar = document.querySelector('nav');
+function toggleSideBar() {
+    if (sideBar.style.width == '0px')
+        sideBar.style.width = '240px';
+    else {
+        sideBar.style.width = '0px';
+        sideBar.style.overflow = 'hidden' /* hides scroll bar */
+    }
+}
 
-// fetch(videoHttp + new URLSearchParams({
-//     key: apiKey,
-//     part: 'snippet',
-//     chart: 'mostPopular',
-//     maxResults: 50,
-//     regionCode: 'IN'
-// }))
-// .then(res => res.json())
-// .then(data => {
-//     data.items.forEach(item => {
-//         getChannelIcon(item);
-//     })
-// })
-// .catch(err => console.log(err));
+const videoContainer = document.querySelector('.video-container');
+let apiKey = 'AIzaSyCFYqgxOa6P18e-PBEy2jvQQDBnuKzxbsE';
+let videoHttp = 'https://www.googleapis.com/youtube/v3/videos?';
+let channelHttp = 'https://www.googleapis.com/youtube/v3/channels?';
 
-// const getChannelIcon = (videoData) => {
-// fetch(channelHttp + new URLSearchParams({
-//     key: apiKey,
-//     part: 'snippet',
-//     id: videoData.snippet.channelId
-// }))
-// .then(res => res.json())
-// .then(data => {
-//     videoData.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-//       makeVideoCard(videoData);
-//     })
-// }
+fetch(videoHttp + new URLSearchParams({
+    key: apiKey,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regionCode: 'IN'
+}))
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
 
-// const makeVideoCard = (data) => {
-//     videoContainer.innerHTML += `
-//     <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
-//         <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
-//         <div class="content">
-//             <img src="${data.channelThumbnail}" class="channel-icon" alt="">
-//             <div class="info">
-//                 <h4 class="title">${data.snippet.title}</h4>
-//                 <p class="channel-name">${data.snippet.channelTitle}</p>
-//             </div>
-//         </div>
-//     </div>` ;
-// }
+const getChannelIcon = (videoData) => {
+fetch(channelHttp + new URLSearchParams({
+    key: apiKey,
+    part: 'snippet',
+    id: videoData.snippet.channelId
+}))
+.then(res => res.json())
+.then(data => {
+    videoData.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+      makeVideoCard(videoData);
+    })
+}
 
-// const searchBar = document.querySelector('#search-bar');
-// const searchBtn = document.querySelector('#search-btn');
-// let queryLink = 'https://www.youtube.com/results?search_query=';
+const makeVideoCard = (data) => {
+    videoContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>` ;
+}
 
-// searchBtn.addEventListener('click', () => {
-//     if(searchBar.value.length) {
-//         console.log(searchBar.value);
-//         location.href = queryLink + searchBar.value;
-//     }
-// });
+searchBar = document.querySelector('#search-bar');
+const searchBtn = document.querySelector('#search-btn');
+let queryLink = 'https://www.youtube.com/results?search_query=';
+
+searchBtn.addEventListener('click', () => {
+    if(searchBar.value.length) {
+        console.log(searchBar.value);
+        location.href = queryLink + searchBar.value;
+    }
+});
